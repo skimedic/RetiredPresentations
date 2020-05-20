2e25cb6c96b5087eb86d246b9800608768ebad9a
skimedic <skimedic@gmail.com>
2020-05-20 09:25:55 -0400

    Moving older presentations

---
+++ Patterns_Old/PatternsExamples/C_Behavioral/Command/IDemoCommand.cs
@@ -0,0 +1,14 @@
+﻿// Copyright Information
+// =============================
+// PatternsExamples - IDemoCommand.cs
+// All samples copyright Philip Japikse 
+// http://www.skimedic.com 19/06/2017
+// See License.txt for more information
+// =============================
+namespace PatternsExamples.C_Behavioral.Commands
+{
+    public interface IDemoCommand
+    {
+        void Execute();
+    }
+}
